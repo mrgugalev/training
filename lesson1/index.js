65c0d2b3de924e641987d62f90e808225f77f938
mrgugalev <maxgugal@mail.ru>
2019-04-03 20:07:15 +0300

    first

---
+++ lesson1/index.js
@@ -0,0 +1,25 @@
+'use strict';
+let money = prompt('Ваш бюджет на месяц?', ''),
+  time = prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD'),
+  firstQuestion = prompt('Введите обязательную статью расходов в этом месяце:', ''),
+  firstAnswer = prompt('Во сколько обойдемся: ', '0'),
+  secondQuestion = prompt('Введите обязательную статью расходов в этом месяце:', ''),
+  secondAnswer = prompt('Во сколько обойдемся: ', '0');
+  
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {}, 
+    optionalExpenses: {},
+    income: '',
+    savings: false
+}
+
+appData.expenses[firstQuestion] = firstAnswer;
+appData.expenses[secondQuestion] = secondAnswer;
+
+function getBudget(i) {
+  return appData.budget / 30 * i;
+}
+getBudget(1);
+
