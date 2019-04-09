b62a5684b3286fb14cd50fbf67b23ba1fe108f40
mrgugalev <maxgugal@mail.ru>
2019-04-09 17:59:39 +0300

    primerno

--- lesson3/lesson3(complicated)/index.js
+++ lesson3/lesson3(complicated)/index.js
@@ -1,3 +1,24 @@
 let str = 'урок-3-был слишком легким';
 document.body.innerHTML += (str[0].toUpperCase() + str.slice(1).replace(/-/g, ' ').replace(' легким', '') + '</br>' );
 console.log(str.slice(str.indexOf('легким')).replace('им', 'о'));
+
+let summ = 0;
+let arr = [20, 33, 1, 'Человек', 2, 3];
+let delet = (arr.indexOf('Человек'));
+(arr.splice(delet,1));
+console.log(arr);
+for(let i = 0; i < arr.length; i++) {
+    summ += Math.pow(arr[i], 3);
+}
+console.log(summ);
+
+
+function anonimus(i) {
+  if(typeof i === 'string') {
+    document.body.innerHTML += i.replace(/\s/g, '').slice(0, 50) + '...';
+  } else {
+    alert('Это не строка!');
+  }
+}
+anonimus(' 2394588932475982347589273495872-49851-09485190485917435728943758923  ');
+
