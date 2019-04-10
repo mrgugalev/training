7a51bced03e82f9259cb24e706f941f32f6115c1
mrgugalev <maxgugal@mail.ru>
2019-04-10 09:59:37 +0300

    ispravlenoe

--- lesson3/lesson3(main)/index.js
+++ lesson3/lesson3(main)/index.js
@@ -21,8 +21,18 @@ let appData = {
 }
 
 function detectLevel() {
+    if(appData.moneyPerDay < 100) {
+      console.log('Минимальный уровень достатка');
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+      console.log('Средний уровень достатка');
+    } else if (appData.moneyPerDay > 2000) {
+      console.log('Высокий уровень достатка');
+    } else {
+      console.log('ошибка ввода');
+    }
+}
 
-  function  chooseExpenses(){    // корректность ввода расходов
+function  chooseExpenses(){    // корректность ввода расходов
     for(let i = 0; i < 2; i++) {
       let a = prompt('Введите обязательную статью расходов в этом месяце:', ''),
           b = prompt('Во сколько обойдемся: ', '');
@@ -36,11 +46,10 @@ function detectLevel() {
       }
     }
     
-  }
-  chooseExpenses();  
+}
+chooseExpenses();  
 
-
-  function checkSavings(){       // накопления
+function checkSavings(){       // накопления
     if (appData.savings == true) {
       let save = +prompt("Какова сумма накоплений?");
       let percent = +prompt("Под какой процент?");
@@ -48,27 +57,14 @@ function detectLevel() {
       appData.monthIncome = save/100/12*percent;
       alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
     }
-  }
-  checkSavings();
-
-
-  function detectDayBudget() {   // расчет бюджета в день
-    appData.moneyPerDay =(appData.budget / 30).toFixed(1);
-    if(appData.moneyPerDay < 1000){
-      console.log('you poor');
-    } else if (appData.moneyPerDay > 1000 && appData.moneyPerDay < 10000){
-      console.log ('you have good money, but not rich');
-    } else if (appData.moneyPerDay > 10000){
-      console.log('you rich');
-    } else {
-      console.log ('error');
-    }   
-    console.log(appData.moneyPerDay);
-  }
-  detectDayBudget();
 }
+checkSavings();
 
-
+function detectDayBudget() {   // расчет бюджета в день
+    appData.moneyPerDay =(appData.budget / 30).toFixed(1);
+    alert('Ежедневный бюджет ' + appData.moneyPerDay);
+}
+detectDayBudget();
 
 function chooseOptExpenses() {
   for(let i = 0; i < 3; i++) {
